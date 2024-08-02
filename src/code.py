ecaac5beaacd357c02a80eff96f62107eeed5821
AKSHAY2126 <akshaysingh5000@gmail.com>
2024-08-02 18:19:01 +0530

    feat: checking new thing-1

--- src/code.py
+++ src/code.py
@@ -1,7 +1,7 @@
-#This is python code
+#This is python code-1
 def add(a,b):
      return a + b
 
 def test_add():
    assert add(1,2) == 3
-   assert add(1,-1) == 0 
+   assert add(1,-1) == 0  # type: ignore
